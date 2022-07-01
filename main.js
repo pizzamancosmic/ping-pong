8964ad0d5b4cf40701f85b7e4bed22ce46b790fd
pizzamancosmic <88450689+pizzamancosmic@users.noreply.github.com>
2022-07-01 17:48:34 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -141,3 +141,11 @@ function midline(){
     }
 }
 
+function setup() {
+  var canvas = createCanvas(700, 600);
+  canvas.parent('canvas');
+
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+}
